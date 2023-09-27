01fe717d4dc4addeb830bd7c03fd98141a7dd499
kiruke <kiruke2525@gmail.com>
2023-09-27 23:56:03 +0900

    add:コメントテーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   has_many :bookmarks, dependent: :destroy 
+  has_many :items,     dependent: :destroy  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_26_161737) do
+ActiveRecord::Schema.define(version: 2023_09_27_144718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2023_09_26_161737) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_bookmarks_on_item_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -45,4 +55,6 @@ ActiveRecord::Schema.define(version: 2023_09_26_161737) do
 
   add_foreign_key "bookmarks", "items"
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
 end
